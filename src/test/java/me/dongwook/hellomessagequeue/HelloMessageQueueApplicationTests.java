f4f7aa37f755dade6a9718b4f59b45e20a300fa5
dongwook <dongzzi101@gmail.com>
2025-04-26 19:51:43 +0900

    first commit

---
+++ src/test/java/me/dongwook/hellomessagequeue/HelloMessageQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dongwook.hellomessagequeue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloMessageQueueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
